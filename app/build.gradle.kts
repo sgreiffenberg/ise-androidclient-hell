08d9d85e4e56f30f0b246cd799ffce0a936e380a
greiffenberg <steffen.greiffenberg@ba-dresden.de>
2024-01-03 10:05:02 +0100

    Retrofit entfernt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,25 +53,28 @@ android {
 
 dependencies {
 
-    // Import the Compose BOM
     implementation(platform("androidx.compose:compose-bom:2023.06.01"))
-    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.activity:activity-compose:1.8.2")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.core:core-ktx:1.10.1")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
     implementation("androidx.lifecycle:lifecycle-runtime-compose:2.6.2")
     implementation("androidx.compose.runtime:runtime-livedata:1.5.4")
 
-    // Retrofit
+    /* Retrofit
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
-    implementation("io.coil-kt:coil-compose:2.4.0")
+    implementation("io.coil-kt:coil-compose:2.4.0")*/
+
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
-    testImplementation("junit:junit:4.12")
+    implementation("com.squareup.okhttp3:okhttp:4.11.0")
+
+    testImplementation("junit:junit:4.13.2")
 
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.06.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
@@ -80,7 +83,4 @@ dependencies {
 
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
-
-    implementation("com.squareup.okhttp3:okhttp:4.11.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
 }
